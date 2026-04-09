a124b2689eef624d30fe625ee8907f5df1e3a4c2
iccarvalho <igccarvalho06@outlook.com>
2026-04-09 19:46:55 -0300

    Interface

---
+++ Interface/src/Checkout.java
@@ -0,0 +1,14 @@
+public class Checkout {
+    public static void main(String[] args) {
+        Pagamento pagamento;
+
+        pagamento = new CartaoCredito();
+        pagamento.autorizar(700); // polimorfismo
+        pagamento.exibirComprovante(); // polimorfismo
+
+        pagamento = new Pix();
+        pagamento.autorizar(800); // polimorfismo
+        pagamento.exibirComprovante(); // polimorfismo
+
+    }
+}
